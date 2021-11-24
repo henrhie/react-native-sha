6bf8e3931b96e93d3962df0698f877f0aaa47d96
Henry Ansah <henryansh083@gmail.com>
2021-11-24 10:37:36 +0000

    added test scripts

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -1,14 +1,4 @@
-// /**
-//  * @format
-//  */
-
-// import 'react-native';
-// import React from 'react';
-// import App from '../App';
-
-// // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
-
-// it('renders correctly', () => {
-//   renderer.create(<App />);
-// });
+/* eslint-disable prettier/prettier */
+it('is a string', () => {
+  expect('hello').toEqual('hello');
+});
---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+it('is a string', () => {
+  expect('hello').toEqual('hello');
+});
