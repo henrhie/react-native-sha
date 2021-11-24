0f355f092a3e20d2a10089bd06a8589ba2a21ea8
Henry Ansah <henryansh083@gmail.com>
2021-11-24 10:21:14 +0000

    remove test scripts

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -1,14 +1,14 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// import 'react-native';
+// import React from 'react';
+// import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
